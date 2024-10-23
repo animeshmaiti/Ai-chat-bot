0cec4bc5e15a7bc0d4cc2a9a58d2064fbbcf67f8
Animesh Maiti <a.maiti770@gmail.com>
2024-10-23 20:22:48 +0530

    proxy setup for connect backend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/matrix': {
+        target: 'http://localhost:8080',  // Your backend server URL
+        changeOrigin: true,
+        secure: false,   // Disable SSL verification if using HTTP
+        rewrite: (path) => path.replace(/^\/matrix/, '/prompt'),
+      },
+    },
+  },
+});
