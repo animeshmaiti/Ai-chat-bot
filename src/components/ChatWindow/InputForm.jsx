78a731d2251e89e15c298f0c6978a20d2a1ed35b
Animesh Maiti <a.maiti770@gmail.com>
2024-10-24 11:04:42 +0530

    input textarea styling

--- src/components/ChatWindow/InputForm.jsx
+++ src/components/ChatWindow/InputForm.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import SendIcon from '@mui/icons-material/Send';
 
 export const InputForm = () => {
+  const [inputValue, setInputValue] = React.useState('');
+  const [rows, setRows] = React.useState(1);
+  const maxRows = 5;
+  const handleChange = (e) => {
+    const lineBreaks = e.target.value.split('\n').length; // Count number of lines
+    const newRows = Math.min(lineBreaks, maxRows); // Set the rows, limiting to maxRows
+    setRows(newRows); // Adjust rows dynamically
+    setInputValue(e.target.value); // Set the input value
+  };
+  
   return (
     <div className="input-container">
-      <form className="form-container" onsubmit="handleSubmit(event)">
+      <form className="form-container" onSubmit="handleSubmit(event)">
         <div className="input-wrapper">
-          <input
-            type="text"
+          <textarea
             className="input-field"
             placeholder="Send a message"
-            value=""
+            value={inputValue}
+            onChange={handleChange}
+            rows={rows}
           />
           <button className="send-button" type="submit">
             <SendIcon />
