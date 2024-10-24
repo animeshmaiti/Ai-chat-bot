6772613ef11318b4594eade5742b215913d79c04
Animesh Maiti <a.maiti770@gmail.com>
2024-10-24 11:03:06 +0530

    loading state

--- src/components/hook/useGetResponse.js
+++ src/components/hook/useGetResponse.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const useGetResponse = () => {
-    const getResponse = async (prompt,model_path,num_tokens,temp) => {
-        try {
-          const response = await fetch('/prompt', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ prompt,model_path,num_tokens,temp}),
-          });
-          const data = await response.json();
-    
-          if (!response.ok) {
-            throw new Error(data.error);
-          }
-          return data.received_answer;
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      };
-    
-      return {getResponse};
+  const [loading, setLoading] = useState(false);
+  const getResponse = async (prompt, model_path, num_tokens, temp) => {
+    setLoading(true);
+    try {
+      const response = await fetch('/prompt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt, model_path, num_tokens, temp }),
+      });
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error);
+      }
+      return data.received_answer;
+    } catch (error) {
+      console.error('Error:', error);
+    }finally {
+      setLoading(false);
+    }
+  };
+
+  return { getResponse, loading };
 }
